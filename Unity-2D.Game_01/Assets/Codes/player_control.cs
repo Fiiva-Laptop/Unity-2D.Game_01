758b27d86c925873a003a2dd94928459a4a387dc
Fiiva-Desktop <56074797+Fiiva-Desktop@users.noreply.github.com>
2020-09-28 00:33:21 +0800

    建立物件及控制角色左右移動

--- Unity-2D.Game_01/Assets/Codes/player_control.cs
+++ Unity-2D.Game_01/Assets/Codes/player_control.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class player_control : MonoBehaviour
 {
+    public float MoveSpeed = (float)10;
+
+    private Rigidbody2D mRigidbody2D;
+    private Animation Animation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            mRigidbody2D.velocity = new Vector2(-MoveSpeed, 0);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            mRigidbody2D.velocity = Vector2.zero;
+        }
+            
+            
+        if (Input.GetKeyDown(KeyCode.RightArrow)) 
+        {
+            mRigidbody2D.velocity = new Vector2(MoveSpeed, 0);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            mRigidbody2D.velocity = Vector2.zero;
+        }
+
     }
 }
