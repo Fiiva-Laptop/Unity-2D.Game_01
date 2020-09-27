e8a65e4c8cfcaab52fdef6f4d5a1bbb60e63ed7e
Fiiva-Desktop <56074797+Fiiva-Desktop@users.noreply.github.com>
2020-09-28 01:23:50 +0800

    新增角色移動動畫

---
+++ Unity-2D.Game_01/Assets/Scripts/Player_Control.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Control : MonoBehaviour
+{
+    public float MoveSpeed = 3.5f;
+
+    private Vector2 MoveDir;
+
+    private Rigidbody2D mRigidbody2D;
+    private Animator mAnimator;
+    private SpriteRenderer mSpriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRigidbody2D = GetComponent<Rigidbody2D>();
+        mAnimator = GetComponent<Animator>();
+        mSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            MoveDir.x = -MoveSpeed;
+            mAnimator.SetFloat("Animator_MoveSpeed", 1);
+            mSpriteRenderer.flipX = true;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            MoveDir.x = MoveSpeed;
+            mAnimator.SetFloat("Animator_MoveSpeed", 1);
+            mSpriteRenderer.flipX = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            MoveDir = Vector2.zero;
+            mAnimator.SetFloat("Animator_MoveSpeed", 0);
+        }
+
+        MoveDir.y = mRigidbody2D.velocity.y;
+        mRigidbody2D.velocity = MoveDir;
+    }
+}
