a6ac24d577a53e699fe9fc134fc63b81bbcaf349
Fiiva-Desktop <56074797+Fiiva-Desktop@users.noreply.github.com>
2020-09-28 18:05:50 +0800

    新增旗子來產生顯示金幣
    將先前的金幣變更為不顯示，直到碰觸其子後才顯示金幣

---
+++ Unity-2D.Game_01/Assets/Scripts/Trigger_Flag.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_Flag : MonoBehaviour
+{
+    public GameObject[] activeObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < activeObjects.Length; i++)
+        {
+            Debug.Log("Coin " + i + ": Disable");
+            activeObjects[i].SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            for (int i=0; i<activeObjects.Length; i++)
+            {
+                Debug.Log("Coin " + i + ": Enable");
+                activeObjects[i].SetActive(true);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
